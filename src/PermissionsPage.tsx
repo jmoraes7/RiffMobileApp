8c35fcc6da757b010d752fe70c8efbf701e59f00
Julian Moraes <jam869@cornell.edu>
2022-08-06 15:21:54 +0700

    fixing title

--- src/PermissionsPage.tsx
+++ src/PermissionsPage.tsx
@@ -40,7 +40,7 @@ export function PermissionsPage({ navigation }: Props): React.ReactElement {
   return (
     <View style={styles.container}>
       <Image source={BANNER_IMAGE} style={styles.banner} />
-      <Text style={styles.welcome}>Welcome to{'\n'}Vision Camera.</Text>
+      <Text style={styles.welcome}>Welcome to{'\n'}Riff.</Text>
       <View style={styles.permissionsContainer}>
         {cameraPermissionStatus !== 'authorized' && (
           <Text style={styles.permissionText}>
